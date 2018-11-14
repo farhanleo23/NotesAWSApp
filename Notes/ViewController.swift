03df20cbe8f509b6e39a734580a4a3adbe4edae3
farhan hussain <farhanleo23@gmail.com>
2018-11-14 23:46:17 +0100

    Notes app which uses AWS

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Farhan Hussain on 11/14/18.
+//  Copyright © 2018 maypalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
